5f0f6e6ddef3b4bfba4fd8174bc94caa24fc8090
Ayanwale Mofiyinfoluwa Praise <praise207@yahoo.com>
2023-04-26 17:01:57 +0100

    create react app adjutment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
+import logo from "../images/logo.svg";
+
+
+
 export default function Header() {
     return (
         <header>
             <nav className = "nav">
-                <img className = "nav-logo" src = "./react-logo.png" alt = "" />
+                <img className = "nav-logo" src = {logo} alt = "" />
                 <ul className = "nav-items">
                     <li><a href="">Pricing</a></li>
                     <li><a href="">About</a></li>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import "./css/index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
